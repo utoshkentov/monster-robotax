ca9b8a0f7ae82b8b781a8999cc3a57a6e521ec3d
Umrzoq-8855 <60574959+Umrzoq-8855@users.noreply.github.com>
2020-04-15 22:33:46 +0500

    Updates

---
+++ precache-manifest.934469d8c65b7fb5f0a8c36cb2095771.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ac7cf8d424e689b7425ffaeb4b1fd8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ff21c2bc32e75dd88f2c",
+    "url": "/monsters-rolodex/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "f5e215eebd79732c9258",
+    "url": "/monsters-rolodex/static/css/main.c522dc40.chunk.css"
+  },
+  {
+    "revision": "ff21c2bc32e75dd88f2c",
+    "url": "/monsters-rolodex/static/js/2.58171e3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.58171e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5e215eebd79732c9258",
+    "url": "/monsters-rolodex/static/js/main.b390d1e1.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
